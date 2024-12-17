077ddbf6f372c8fa25e787b8b2e47660e46e9664
boumahdimanel <145130591+boumahdimanel@users.noreply.github.com>
2024-12-17 14:24:56 +0100

    Update SolutionTest.java

--- projet/src/Test/SolutionTest.java
+++ projet/src/Test/SolutionTest.java
@@ -15,27 +15,7 @@ class SolutionTest {
 	
 
 	    @Test
-	    void testCalculerFitness() {
-	        Ville paris = new Ville("Paris", "75000", 2000000, 105.4, "75", 2.3522, 48.8566);
-	        Ville lyon = new Ville("Lyon", "69000", 500000, 47.87, "69", 4.8357, 45.7640);
-	        Ville marseille = new Ville("Marseille", "13000", 800000, 240.62, "13", 5.3698, 43.2965);
-
-	        List<Ville> circuit = Arrays.asList(paris, lyon, marseille);
-	        Solution solution = new Solution(circuit);
-
-	        double expectedFitness = 1.0 / (paris.distanceTo(lyon) + lyon.distanceTo(marseille) + marseille.distanceTo(paris));
-	        assertEquals(expectedFitness, solution.getFitness(), 0.0001);
-	    }
-
-	    @Test
-	    void testAjouterVille() {
-	        Solution solution = new Solution();
-	        Ville paris = new Ville("Paris", "75000", 2000000, 105.4, "75", 2.3522, 48.8566);
-	        
-	        solution.ajouterVille(paris);
-	        assertEquals(1, solution.getCircuit().size());
-	        assertEquals(paris, solution.getCircuit().get(0));
-	    }
+	   
 }
 
 
