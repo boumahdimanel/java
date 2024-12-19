ef0cf4b314fc4dd013d7cbf265324677ddefbdab
boumahdimanel <145130591+boumahdimanel@users.noreply.github.com>
2024-12-19 16:27:59 +0100

    Update Solution.java

--- projet/src/model/Solution.java
+++ projet/src/model/Solution.java
@@ -2,19 +2,25 @@ package model;
 
 import java.util.List;
 import java.util.ArrayList;
+//import java.sql.Connection;
+//import java.sql.SQLException;
+//import databaseConnection.DatabaseConnection;
 
 public class Solution {
     private List<Ville> circuit;
     private double fitness;
+   // private Connection connection;
 
     public Solution() {
         this.circuit = new ArrayList<>();
         this.fitness = 0.0;
+       // this.connection = DatabaseConnection.getConnection();
     }
 
     public Solution(List<Ville> circuit) {
-        this.circuit = new ArrayList<>(circuit);
-        this.fitness = calculerFitness();
+    	 this.circuit = new ArrayList<>(circuit);
+        // this.connection = DatabaseConnection.getConnection();
+         this.fitness = calculerFitness();
     }
 
     public List<Ville> getCircuit() {
@@ -31,19 +37,16 @@ public class Solution {
     }
 
     public double calculerFitness() {
-        double distance = 0.0;
-        for (int i = 0; i < circuit.size() - 1; i++) {
-            distance += (circuit.get(i)).distanceTo(circuit.get(i + 1));
-        }
-        // Ajouter la distance de retour à la ville de départ
-        distance += circuit.get(circuit.size() - 1).distanceTo(circuit.get(0));
-        this.fitness = 1.0 / distance; // Plus la distance est courte, plus la fitness est élevée
-        return this.fitness;
-    }
-
-    public void ajouterVille(Ville ville) {
-        circuit.add(ville);
-        this.fitness = calculerFitness();
+    	 double distance = 0.0;
+         
+             for (int i = 0; i < circuit.size() - 1; i++) {
+                 distance += circuit.get(i).distanceTo(circuit.get(i + 1));
+             }
+             // Ajouter la distance de retour à la ville de départ
+             distance +=circuit.get(circuit.size() - 1).distanceTo(circuit.get(0));
+             this.fitness = 1.0 / distance; // Plus la distance est courte, plus la fitness est élevée
+         
+         return this.fitness;
     }
 
     @Override
